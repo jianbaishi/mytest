e32d6b5381370d174a99435bd53a4bd8cb5433cb
Adai <wendai_chen@sina.com>
2017-04-24 09:31:20 +0000

    read data

--- in.py
+++ in.py
@@ -175,18 +175,21 @@ with tf.Session() as sess:
 import tensorflow as tf
 import numpy
 
-def get_file_line(filename):
-    myfile = open(filename) 
-    lines = len(myfile.readlines()) 
-    return lines
+def get_file_line(filename_s):
+    line_s = 0
+    file_num = len(filename_s)
+    for index in range(file_num):
+        myfile = open(filename_s[index]) 
+        line = len(myfile.readlines())
+        line_s += line - 1
+        print filename_s[index], line
+    print line_s
+    return line_s
 
 def read(filenames, batch_size):
     # 生成一个先入先出队列和一个QueueRunner,生成文件名队列  
 		
     filename_queue = tf.train.string_input_producer(filenames, shuffle=False)
-    print(filename_queue.queue_ref)
-    lines = get_file_line(filename="data/sh_hq_000001_2011.csv")
-    print(lines)
     
     # 定义Reader  
     reader = tf.TextLineReader(skip_header_lines=1)
@@ -195,31 +198,203 @@ def read(filenames, batch_size):
     record_defaults = [['null'], ['null'], ['null'], ['null'], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0]]
     col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13 = tf.decode_csv(value,record_defaults=record_defaults)
     features = tf.stack([col3, col1, col2, col4])
-    output = tf.stack([col5, col6, col7, col8, col9, col10, col11, col12, col13]) 
+    features_1 = tf.stack([col5, col6, col7, col8, col9, col10, col11, col12, col13])
+    in_put_f = tf.stack([col5, col6, col7, col8, col9, col10, col13])
+    out_put_f = tf.stack([col10])
     tf.set_random_seed(0)
-    example_batch, label_batch = tf.train.batch([features,output], batch_size=batch_size, capacity=2000, num_threads=1)
-    print(example_batch, label_batch)
+    example_batch, label_batch, in_put, out_put = tf.train.batch([features, features_1, in_put_f, out_put_f], batch_size=batch_size, capacity=2000, num_threads=1)
+    
     # 运行Graph  
     with tf.Session() as sess:  
         coord = tf.train.Coordinator()  #创建一个协调器，管理线程  
         threads = tf.train.start_queue_runners(coord=coord)  #启动QueueRunner, 此时文件名队列已经进队。  
-        e_val,l_val = sess.run([example_batch, label_batch])  
+        e_val, l_val, in_val, out_val = sess.run([example_batch, label_batch, in_put, out_put])  
         #print e_val,l_val  
         coord.request_stop()  
         coord.join(threads)
-    return e_val,l_val
+    return e_val, l_val, example_batch, label_batch, in_val, out_val, in_put, out_put
+
+def get_info_form_file(filenames):
+    lines = get_file_line(filename_s=filenames)
+    e_val, l_val, example_batch, label_batch = read(filenames, lines)
+    return e_val,l_val,example_batch,label_batch,in_val, out_val
+
+#从文件读取信息
+def get_info_form_file_and_slice(filenames, begin, size, diff_size):
+    lines = get_file_line(filename_s=filenames)
+    e_val_old, l_val_old, example_batch_old, label_batch_old, in_val_old, out_val_old, in_put_old, out_put_old = read(filenames, lines)
+    
+    example_batch = tf.slice(example_batch_old, [begin, 0], [size, 4])
+    label_batch = tf.slice(label_batch_old, [begin, 0], [size, 9])
+    in_put = tf.slice(in_put_old, [begin, 0], [size - diff_size, 7])
+    out_put = tf.slice(out_put_old, [begin, 0], [size, 1])
+    with tf.Session() as sess:  
+        coord = tf.train.Coordinator()  #创建一个协调器，管理线程  
+        threads = tf.train.start_queue_runners(coord=coord)  #启动QueueRunner, 此时文件名队列已经进队。  
+        e_val,l_val, in_val, out_val = sess.run([example_batch, label_batch, in_put, out_put])  
+        #print e_val,l_val  
+        coord.request_stop()  
+        coord.join(threads)
+
+    return e_val, l_val, example_batch, label_batch, in_val, out_val, in_put, out_put
+
+#输入数据合并
+def get_in_concat_data(in1, in2, in3, in4):
+    input_1 = tf.concat([in1, in2], 1)
+    input_2 = tf.concat([input_1, in3], 1)
+    input_3 = tf.concat([input_2, in4], 1)
+    with tf.Session() as sess:  
+        coord = tf.train.Coordinator()  #创建一个协调器，管理线程  
+        threads = tf.train.start_queue_runners(coord=coord)  #启动QueueRunner, 此时文件名队列已经进队。  
+        input_3_val = sess.run([input_3])  
+        #print e_val,l_val  
+        coord.request_stop()  
+        coord.join(threads)
+    return input_3_val, input_3
+
+def get_in_data_last5dat(int_put, begin, size):
+    int_put_data = tf.slice(int_put, [begin, 0], [size, 4])
+    with tf.Session() as sess:  
+        coord = tf.train.Coordinator()  #创建一个协调器，管理线程  
+        threads = tf.train.start_queue_runners(coord=coord)  #启动QueueRunner, 此时文件名队列已经进队。  
+        int_put_val = sess.run([int_put_data])  
+        #print e_val,l_val  
+        coord.request_stop()  
+        coord.join(threads)
+    return int_put_val, int_put_data
+
+# 5day后的收盘价
+def get_out_data_last5day(out, lines):
+    out_put = tf.gather(out,[1, 2, 3, 4, 5])
+    out_put_tra = tf.transpose(out_put)
+    print(out_put_tra)
+    for index in range(1,lines - 5):
+        one_out = tf.gather(out,[1 + index, 2 + index, 3 + index, 4 + index, 5 + index])
+        one_out_tra = tf.transpose(one_out)
+        out_put_tra = tf.concat([out_put_tra, one_out_tra], 0)
+        #print(one_out)
+        #print(one_out_tra)
+    print(out_put_tra)
+    with tf.Session() as sess:  
+        coord = tf.train.Coordinator()  #创建一个协调器，管理线程  
+        threads = tf.train.start_queue_runners(coord=coord)  #启动QueueRunner, 此时文件名队列已经进队。  
+        out_put_tra_val = sess.run([out_put_tra])
+        print(out_put_tra_val)
+        #print e_val,l_val  
+        coord.request_stop()  
+        coord.join(threads)
+    return out_put_tra_val, out_put_tra
 
 
-filenames = ["data/sh_hq_000001_2011.csv", "data/sh_hq_000001_2012.csv"]
-val1, val2 = read(filenames, batch_size=3)
-print(val1, val2)
 
-numpy.set_printoptions(threshold='nan')
-filenames = ["data/sz_hq_300188_2011.csv", "data/sz_hq_300188_2012.csv"]
-val1_300188, val2_300188 = read(filenames, batch_size=2)
-print("%s" %val1_300188)
-print(val2_300188)
-#print("%.3f" %val2_300188)
+#numpy.set_printoptions(threshold='nan')
 
-a=[1.1,2.2,3.3]
-print(a)
+filenames_000001 = ["data/sh_hq_000001_2011.csv", "data/sh_hq_000001_2012.csv", "data/sh_hq_000001_2013.csv", "data/sh_hq_000001_2014.csv", \
+    "data/sh_hq_000001_2015.csv", "data/sh_hq_000001_2016.csv"]
+filenames_399001 = ["data/sz_hq_399001_2011.csv", "data/sz_hq_399001_2012.csv", "data/sz_hq_399001_2013.csv", "data/sz_hq_399001_2014.csv", \
+    "data/sz_hq_399001_2015.csv", "data/sz_hq_399001_2016.csv"]
+filenames_399006 = ["data/sz_hq_399006_2011.csv", "data/sz_hq_399006_2012.csv", "data/sz_hq_399006_2013.csv", "data/sz_hq_399006_2014.csv", \
+    "data/sz_hq_399006_2015.csv", "data/sz_hq_399006_2016.csv"]
+filenames_300188 = ["data/sz_hq_300188_2011.csv", "data/sz_hq_300188_2012.csv", "data/sz_hq_300188_2013.csv", "data/sz_hq_300188_2014.csv", \
+    "data/sz_hq_300188_2015.csv", "data/sz_hq_300188_2016.csv"]
+
+lines_000001 = get_file_line(filename_s=filenames_000001)
+lines_300188 = get_file_line(filename_s=filenames_300188)
+begin = lines_000001 - lines_300188 + 1
+#后面5个数据无法测试
+test_day = 5
+input_size = lines_300188 - 1
+output_size = input_size
+
+#e_val_000001, l_val_000001, example_batch_000001, label_batch_000001 = get_info_form_file(filenames_000001)
+#e_val_399001, l_val_399001, example_batch_399001, label_batch_399001 = get_info_form_file(filenames_399001)
+#e_val_399006, l_val_399006, example_batch_399006, label_batch_399006 = get_info_form_file(filenames_399006)
+#e_val_300188, l_val_300188, example_batch_300188, label_batch_300188 = get_info_form_file(filenames_300188)
+
+#300188 过滤第一个
+e_val_000001, l_val_000001, example_batch_000001, label_batch_000001, in_val_000001, \
+    out_val_000001, in_put_000001, out_put_000001 = \
+    get_info_form_file_and_slice(filenames=filenames_000001, begin=begin, size=input_size, diff_size=0)
+e_val_399001, l_val_399001, example_batch_399001, label_batch_399001, in_val_399001, \
+    out_val_399001, in_put_399001, out_put_399001 = \
+    get_info_form_file_and_slice(filenames=filenames_399001, begin=begin, size=input_size, diff_size=0)
+e_val_399006, l_val_399006, example_batch_399006, label_batch_399006, in_val_399006, \
+    out_val_399006, in_put_399006, out_put_399006 = \
+    get_info_form_file_and_slice(filenames=filenames_399006, begin=begin, size=input_size, diff_size=0)
+e_val_300188, l_val_300188, example_batch_300188, label_batch_300188, in_val_300188, \
+    out_val_300188, in_put_300188, out_put_300188 = \
+    get_info_form_file_and_slice(filenames=filenames_300188, begin=1, size=input_size, diff_size=0)
+
+#print(e_val_000001, l_val_000001)
+#print(e_val_399001, l_val_399001)
+#print(e_val_399006, l_val_399006)
+#print(e_val_300188, l_val_300188)
+#print(in_val_300188, out_val_300188)
+print(in_put_000001, out_put_000001)
+print(in_put_399001, out_put_399001)
+print(in_put_399006, out_put_399006)
+print(in_put_300188, out_put_300188)
+
+input_val, input_data = get_in_concat_data(in1=in_put_000001, in2=in_put_399001, in3=in_put_399006, in4=in_put_300188)
+print(input_data)
+print(input_val)
+
+real_input_val, real_input_data = get_in_data_last5dat(input_data, 0, input_size - 5)
+print(real_input_data)
+
+print("=======================")
+output_data_val, output_data = get_out_data_last5day(out_put_300188, output_size)
+print("=======================")
+#print(out_val_300188)
+
+'''
+in_1 = tf.concat([in_put_000001, in_put_399001], 1)
+in_2 = tf.concat([in_1, in_put_399006], 1)
+in_3 = tf.concat([in_2, in_put_300188], 1)
+#in_1 = tf.tile(in_put_000001, in_put_399001, name=None)
+print(in_1)
+print(in_2)
+print(in_3)
+
+with tf.Session() as sess: 
+    coord = tf.train.Coordinator()  #创建一个协调器，管理线程  
+    threads = tf.train.start_queue_runners(coord=coord)  #启动QueueRunner, 此时文件名队列已经进队。  
+    in3_val = sess.run([in_3]) 
+    print(in3_val)
+    coord.request_stop()  
+    coord.join(threads)
+'''
+
+'''
+lines_000001 = get_file_line(filename_s=filenames_000001)
+
+
+lines_000001 = get_file_line(filename_s=filenames_000001)
+val1_000001, val2_000001 = read(filenames_000001, lines_000001)
+print("================= 000001 =================")
+print(val1_000001, val2_000001)
+print("=================  end   =================")
+
+
+lines_399001 = get_file_line(filename_s=filenames_399001)
+val1_399001, val2_399001 = read(filenames_399001, lines_399001)
+print("================= 399001 =================")
+#print(val1_399001, val2_399001)
+print("=================  end   =================")
+
+
+lines_399006 = get_file_line(filename_s=filenames_399006)
+val1_399006, val2_399006 = read(filenames_399006, lines_399006)
+print("================= 399006 =================")
+#print(val1_399006, val2_399006)
+print("=================  end   =================")
+
+
+lines_300188 = get_file_line(filename_s=filenames_300188)
+val1_300188, val2_300188 = read(filenames_300188, lines_300188)
+print("================= 300188 =================")
+#print(val1_300188, val2_300188)
+print(val1_300188[100], val2_300188[100])
+print("=================  end   =================")
+'''
+
